2f0f1e383cc498036e36ba5cad89b7e29fb3eecf
Francisco Casillas <chiccasillas@gmail.com>
2025-05-02 10:09:15 -0600

    Initial Commit

---
+++ CityExplorerApp/CityExplorerAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CityExplorerAppApp.swift
+//  CityExplorerApp
+//
+//  Created by Francisco Jean on 02/05/25.
+//
+
+import SwiftUI
+
+@main
+struct CityExplorerAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
